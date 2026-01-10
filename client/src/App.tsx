8b61edf41b11d0b9377ff36e6a818ddeb53e7da6
Asilbek <atukhtaev@infolytics.com>
2026-01-10 13:28:09 +0100

    Fix router base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,23 +8,19 @@ import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import MenuPage from "@/pages/MenuPage";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/menu" component={MenuPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <MusicProvider>
           <Toaster />
-          <Router />
+          <Router base="/silkroad">
+            <Switch>
+              <Route path="/" component={Home} />
+              <Route path="/menu" component={MenuPage} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
         </MusicProvider>
       </TooltipProvider>
     </QueryClientProvider>
