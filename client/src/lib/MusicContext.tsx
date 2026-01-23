e6d4e061b948f4b1daa8893ead941a3299a01491
Asilbek <atukhtaev@infolytics.com>
2026-01-23 19:57:51 +0100

    fix: reduce audio volume in MusicContext to improve user experience

--- client/src/lib/MusicContext.tsx
+++ client/src/lib/MusicContext.tsx
@@ -15,7 +15,7 @@ export function MusicProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     audioRef.current = new Audio(backgroundMusic);
     audioRef.current.loop = true;
-    audioRef.current.volume = 0.05;
+    audioRef.current.volume = 0.005;
 
     // Try to autoplay
     const playPromise = audioRef.current.play();
